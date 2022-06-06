505b5da9ec03317c9c8674ea7330f1acd9c7bed1
Melissa Chua <melissachua@macbook-pro.lan>
2022-06-06 12:03:59 +0800

    updates to PSI.js plus new WeatherContainer.js

--- src/components/PSI.js
+++ src/components/PSI.js
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react'; 
-import API from '../API';
+import { useState, useEffect } from 'react';
 import dateFormat from 'dateformat';
+import API from '../API';
+import '../styles/PSI.css';
 
-const PSI = () => {
+const PSI = (props) => {
 
     const [index, setIndex] = useState({});
-    const [time, setTime] = useState('');
+    const [time, setTime] = useState(''); //for checking only
+    const [region, setRegion] = useState('');
 
     const getPSI = async () => {
         const { status, data } = await API.get('/environment/psi');
@@ -19,10 +21,41 @@ const PSI = () => {
 
     useEffect(() => {
         getPSI();
-    }, []);
+        setRegion(props.region);
+        console.log("useEffect has been called") //for checking only
+    }, [props.region]);
+
+    var alertLevel;
+    var airQuality;
+    var psiAdvisory;
+    switch (true) {
+        case (index[region] <= 50):
+            alertLevel = "green";
+            airQuality = "Good";
+            break;
+        case (51 <= index[region] <= 100):
+            alertLevel = "blue";
+            airQuality = "Moderate";
+            break;
+        case (101 <= index[region] <= 200):
+            alertLevel = "yellow";
+            airQuality = "Unhealthy";
+            break;
+        case (201 <= index[region] <= 300):
+            alertLevel = "orange";
+            airQuality = "Very Unhealthy";
+            break;
+        case (300 <= index[region]):
+            alertLevel = "red";
+            airQuality = "Hazardous";
+            break;
+        default:
+            alertLevel = "Error";
+            break;
+    }
 
     return (<>
-        <h2>PSI index</h2> 
+        <h2>PSI index</h2>
         <div className='subheader'>As of {dateFormat(time, "dddd, dS mmmm yyyy, h:MM TT")}</div>
         <ul>
             <li>National: {index.national}</li>
@@ -30,7 +63,49 @@ const PSI = () => {
             <li>South: {index.south}</li>
             <li>East: {index.east}</li>
             <li>West: {index.west}</li>
+            <li>Central: {index.central}</li>
         </ul>
+        <div className='psi-container' id={alertLevel}>
+            <div id='region'>
+                {region.toUpperCase()}
+            </div>
+            <div className='psi-index' id='psi-index'>
+                {index[region]}
+            </div>
+            <div>
+                {airQuality}
+            </div>
+        </div>
+        <div>
+            <table>
+                <thead>
+                    <th>PSI</th>
+                    <th>Air Quality</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>0 - 50</td>
+                        <td>Good</td>
+                    </tr>
+                    <tr>
+                        <td>51 - 100</td>
+                        <td>Moderate</td>
+                    </tr>
+                    <tr>
+                        <td>101 - 200</td>
+                        <td>Unhealthy</td>
+                    </tr>
+                    <tr>
+                        <td>201-300</td>
+                        <td>Very Unhealthy</td>
+                    </tr>
+                    <tr>
+                        <td>Above 300</td>
+                        <td>Hazardous</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </>)
 
 }
