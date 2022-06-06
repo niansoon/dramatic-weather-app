c1bdc239a75ef4c743a4a8f178389058bf5da36a
niansoon <simniansoon@hotmail.com>
2022-06-06 16:09:37 +0800

    Region

--- src/components/Region.js
+++ src/components/Region.js
@@ -1,15 +1,14 @@
 
 const Region = (area) => {
-    var region;
-    switch(area) {
-        //North & North-East together, 15
+    var userRegion;
+    switch (area) {
+        //North & North-East together, 14
         case "Ang Mo Kio":
         case "Hougang":
         case "Punggol":
         case "Seletar":
         case "Sengkang":
         case "Serangoon":
-        case "Central Water Catchment":
         case "Lim Chu Kang":
         case "Mandai":
         case "Sungei Kadut":
@@ -18,24 +17,19 @@ const Region = (area) => {
         case "Pulau Ubin":
         case "Pulau Tekong":
         case "Sembawang":
-            region = "north-northeast"
+            userRegion = "north-northeast"
             break;
-            
-        //South aka Central, 13
-        case "Bishan":
+
+        //South aka Central, 8
         case "Bukit Merah":
-        case "Bukit Timah":
         case "City":
         case "Geylang":
-        case "Kallang":
         case "Marine Parade":
         case "Queenstown":
         case "Southern Islands":
-        case "Toa Payoh":
-        case "Novena":
         case "Sentosa":
         case "Tanglin":
-            region = "south-central"
+            userRegion = "south-central"
             break;
 
         //East, 5
@@ -44,7 +38,7 @@ const Region = (area) => {
         case "Paya Lebar":
         case "Pasir Ris":
         case "Tampines":
-            region = "east"
+            userRegion = "east"
             break;
 
         //West, 14
@@ -62,7 +56,17 @@ const Region = (area) => {
         case "Jurong Island":
         case "Jalan Bahar":
         case "Tuas":
-            region = "west"
+            userRegion = "west"
+            break;
+
+        //Central, 5
+        case "Kallang":
+        case "Toa Payoh":
+        case "Novena":
+        case "Bukit Timah":
+        case "Bishan":
+        case "Central Water Catchment":
+            userRegion = "central"
             break;
     };
 };
