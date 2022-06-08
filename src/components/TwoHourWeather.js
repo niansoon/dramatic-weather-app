1b16952e959b8cc1a19c68994aa29e78139c798c
Farhan <mdfarhansamsuri@gmail.com>
2022-06-08 19:45:58 +0800

    && , ||

--- src/components/TwoHourWeather.js
+++ src/components/TwoHourWeather.js
@@ -7,6 +7,7 @@ const TwoHourWeather = (props) => {
     const [weatherIcon, setWeatherIcon] = useState('');
     const [weather, setWeather] = useState([]);
     const [time, setTime] = useState("");
+    const [temp, setTemp] = useState ({});
 
     const getWeather = async () => {
         const { status, data } = await API.get(
@@ -18,42 +19,50 @@ const TwoHourWeather = (props) => {
         }
     };
 
+    const getTemperature = async () => {
+      const {status, data } = await API.get(
+        "/environment/24-hour-weather-forecast"
+      );
+      if (status === 200) {
+        setTemp(data.items[0].temperature);
+        return(status);
+      }
+    }
+
     const userArea = props.area;
-    const currentWeather = weather.filter((f) => f.area === userArea).map(filtered => filtered.forecast);
+    const currentWeather = weather.filter((f) => f.area === userArea).map(filtered => filtered.forecast); 
 
     useEffect(() => {
         getWeather();
         console.log("weather", weather);
-        console.log("currentWeather", currentWeather);
         console.log("weather-icon", weatherIcon);
 
-
-
         switch (true) {
-            case currentWeather.includes("Thundery"):
+            case currentWeather.includes("Thundery Showers"):
                 setWeatherIcon('thunderstorm-2hr');
                 break;
             case currentWeather.includes("Fair"):
                 setWeatherIcon('sunny-2hr');
                 break;
-            case currentWeather.includes("Cloudy"):
+            case currentWeather.includes("Cloudy" || "Partly Cloudy (Day)"):
                 setWeatherIcon('cloudy-2hr');
                 break;
-            case currentWeather.includes("Showers"):
+            case currentWeather.includes("Showers" && "Moderate Rain"):
                 setWeatherIcon('showers-2hr');
                 break;
-            /* default:
-        setWeatherIcon('sunny');
-        break; */}
+            case currentWeather.includes("Light Rain"):
+                setWeatherIcon('light-rain-2hr');
+                break;
+              }
 
-
+              console.log("currentWeather", currentWeather);
     }, [userArea, time]);
 
     return (
         <div>
             <h2>2 Hour Weather</h2>
             {userArea}
-            <div className='weather-icon' id={weatherIcon}></div>
+            <div className='weather-icon-2hr' id={weatherIcon}></div>
             <p>{currentWeather}</p>
         </div>
     );
